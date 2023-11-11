31903bb2a086cd35d893256552e25fb7e16fcded
江夏尧 <3446798488@qq.com>
2023-11-11 17:49:32 +0800

    feat: 提供分片大小参数

--- server/router/file.ts
+++ server/router/file.ts
@@ -1,25 +1,43 @@
 import { z } from "zod";
 import { publicProcedure, router } from "../t.js";
-import fs from 'fs-extra'
-import path from 'path'
+import fs from "fs-extra";
+import path from "path";
 
 /** 以分页的方式查看文件夹 */
-const viewDir = async (dir: string, query: { page: number }) => {
-    const list = (await fs.readdir(dir))
-    const slice = await Promise.all(list.slice(query.page * 10, (query.page + 1) * 10).map(async i => {
-        const stat = await fs.stat(path.join(dir, i))
-        return { ...stat, isFile: stat.isFile(), isFIFO: stat.isFIFO(), isDirectory: stat.isDirectory(), name: i }
-    }))
-    return { dir, count: list.length, list: slice };
-}
-
-
+const viewDir = async (dir: string, query: { page: number; size: number }) => {
+    const list = await fs.readdir(dir);
+    const slice = await Promise.all(
+        list
+            .slice(query.page * query.size, (query.page + 1) * query.size)
+            .map(async (i) => {
+                const stat = await fs.stat(path.join(dir, i));
+                return {
+                    ...stat,
+                    isFile: stat.isFile(),
+                    isFIFO: stat.isFIFO(),
+                    isDirectory: stat.isDirectory(),
+                    name: i,
+                };
+            })
+    );
+    return { dir, count: list.length, list: slice, size: query.size };
+};
 
 export const fileRouter = router({
-    listDir: publicProcedure.input(z.object({
-        page: z.number(),
-        dir: z.string()
-    }).nullish()).query(async ({ input: { page, dir } }) => {
-        return viewDir(dir || process.env.HOME, { page })
-    }),
+    listDir: publicProcedure
+        .input(
+            z
+                .object({
+                    page: z.number(),
+                    dir: z.string(),
+                    size: z.number().optional(),
+                })
+                .nullish()
+        )
+        .query(async ({ input: { page, dir, size } }) => {
+            return viewDir(dir || process.env.HOME, {
+                page,
+                size: size ?? 10,
+            });
+        }),
 });
