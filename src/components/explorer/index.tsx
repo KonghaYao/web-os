229801f0a9946bb8cbe263c898be1acd299b1f2c
江夏尧 <3446798488@qq.com>
2023-11-12 09:42:04 +0800

    feat: 自动滚动加载完成

--- src/components/explorer/index.tsx
+++ src/components/explorer/index.tsx
@@ -3,15 +3,19 @@ import { trpc } from "../../api";
 import { SolidListTable } from "../../basic/ListTable";
 export const Explorer = () => {
     const pwd = atom("");
+    const maxCount = atom(0);
     /** 用户输入的属性 */
     const pathString = reflect(() => pwd());
     const folder = usePaginationStack((page, maxPage) => {
-        return trpc.file.listDir.query({ page, dir: pwd() }).then((res) => {
-            maxPage(Math.ceil(res.count / 10));
-            pwd(res.dir);
+        return trpc.file.listDir
+            .query({ page, dir: pwd(), size: 10 })
+            .then((res) => {
+                maxPage(Math.ceil(res.count / 10));
+                maxCount(res.count);
+                pwd(res.dir);
 
-            return res.list;
-        });
+                return res.list;
+            });
     }, {});
     const itemList = reflect(() => folder.dataSlices().flat());
 
@@ -45,6 +49,22 @@ export const Explorer = () => {
                     }
                     console.log();
                 }}
+                scroll={(args) => {
+                    if (
+                        !folder.currentData.loading() &&
+                        typeof args.scrollRatioY === "number" &&
+                        (args.scrollRatioY >= 0.8 || isNaN(args.scrollRatioY))
+                    ) {
+                        // 由于 usePagination 本身设计有 debounce，所以需要快速直接更新状态
+                        const index = folder.currentIndex() + 1;
+                        if (index < 0 || index >= folder.maxPage()) {
+                            return false;
+                        } else {
+                            folder.currentIndex(index);
+                            folder.currentData.refetch();
+                        }
+                    }
+                }}
                 columns={[
                     { field: "name", title: "名称", width: 200, sort: true },
                     {
@@ -54,6 +74,8 @@ export const Explorer = () => {
                         sort: true,
                     },
                 ]}></SolidListTable>
+            {folder.dataSlices().reduce((col, cur) => col + cur.length, 0)}/
+            {maxCount()}
             {folder.currentPage() < folder.maxPage() && (
                 <div onclick={() => folder.next()}>加载更多</div>
             )}
