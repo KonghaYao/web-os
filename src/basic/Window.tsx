0a1a1b631e21bcdeb8fa773723666db7f209c6ec
江夏尧 <3446798488@qq.com>
2023-11-13 21:38:50 +0800

    feat: 重新构建命令行工具

--- src/basic/Window.tsx
+++ src/basic/Window.tsx
@@ -1,13 +1,7 @@
-import { ArrayAtomType, Atom, atom, localSync, reflect } from "@cn-ui/reactive";
+import { Atom, atom, localSync, reflect } from "@cn-ui/reactive";
 import { createDraggable } from "@neodrag/solid";
 import { useFullscreen } from "solidjs-use";
-import {
-    Component,
-    Context,
-    JSXElement,
-    createContext,
-    useContext,
-} from "solid-js";
+import { Component, JSXElement, createContext, useContext } from "solid-js";
 import { Dynamic } from "solid-js/web";
 import { GetContextInner } from "./type-utils";
 import { SystemMenuList } from "../components/system/Menu/SystemMenuList";
@@ -54,10 +48,10 @@ export const Window = (props: {
                         bottom: -Infinity,
                     },
                 }}
-                onclick={() => {
+                onmousedown={() => {
                     system.menuList(menuList());
                 }}
-                class="bg-gray-50/70 shadow-lg shadow-black/25 flex w-[40rem] h-[25rem] flex-col border border-gray-400 rounded overflow-hidden relative cursor-default select-none">
+                class="bg-gray-50/70 shadow-lg shadow-black/25 flex w-[40rem] h-[25rem] flex-col border border-gray-400 rounded-lg overflow-hidden relative cursor-default select-none">
                 <HeaderBar></HeaderBar>
                 {props.children}
             </section>
@@ -72,7 +66,7 @@ export const HeaderBar = () => {
     const fullScreen = useFullscreen(main);
     return (
         <header
-            class="flex h-8 items-center window-handle px-2 select-none"
+            class="flex h-8 flex-none items-center window-handle px-2 select-none"
             ref={header}>
             <div class="flex gap-2 items-center">
                 <button class="h-3 w-3 rounded-full bg-red-400"></button>
--- src/components/terminal/Terminal.tsx
+++ src/components/terminal/Terminal.tsx
@@ -1,40 +1,44 @@
-import { Terminal } from "xterm";
-import "xterm/css/xterm.css";
+import { Terminal } from "@xterm/xterm";
+import "@xterm/xterm/css/xterm.css";
 import { onCleanup, onMount } from "solid-js";
-import { AttachAddon } from "xterm-addon-attach";
+import { AttachAddon } from "@xterm/addon-attach";
 import { atom, localSync } from "@cn-ui/reactive";
-import { FitAddon } from "xterm-addon-fit";
-import { WebLinksAddon } from "xterm-addon-web-links";
+import { FitAddon } from "@xterm/addon-fit";
+import { WebLinksAddon } from "@xterm/addon-web-links";
+import { SearchAddon } from "@xterm/addon-search";
+import { CanvasAddon } from "@xterm/addon-canvas";
 import { theme } from "./theme.ts";
 import { Window } from "../../basic/Window.tsx";
 import { MenuList } from "./MenuList.tsx";
+import { useResizeObserver } from "solidjs-use";
 export function WebTerminal() {
     const dom = atom<HTMLDivElement | null>(null);
     const socketURL = atom("");
     localSync(socketURL, "WEB_OS_SOCKET_URL");
 
-    let term: Terminal;
+    let terminal: ReturnType<typeof createTerminal>;
     onMount(() => {
         do {
             try {
-                term = createTerminal(socketURL());
+                terminal = createTerminal(socketURL());
             } catch (e: unknown) {
                 confirm((e as Error).message);
                 socketURL(
                     () => prompt("请输入 WEB OS 的 WebSocket 端口地址") || ""
                 );
             }
-        } while (!term);
-        term.open(dom()!);
-        // term.fit();
-        console.log(term);
+        } while (!terminal);
+        terminal.term.open(dom()!);
     });
     onCleanup(() => {
-        term.dispose();
+        terminal?.term.dispose();
+    });
+    useResizeObserver(dom, () => {
+        if (terminal) terminal.fit.fit();
     });
     return (
         <Window name="terminal" menuList={MenuList}>
-            <div ref={dom}></div>;
+            <div class="h-full w-full" ref={dom}></div>
         </Window>
     );
 }
@@ -53,8 +57,11 @@ const createTerminal = (socketURL: string) => {
 
     term.focus();
     const ws = new WebSocket(socketURL);
+    const fit = new FitAddon();
     term.loadAddon(new AttachAddon(ws));
-    term.loadAddon(new FitAddon());
+    term.loadAddon(fit);
     term.loadAddon(new WebLinksAddon());
-    return term;
+    term.loadAddon(new SearchAddon());
+    term.loadAddon(new CanvasAddon());
+    return { term, fit, ws };
 };
